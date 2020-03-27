1f14056f83b6dd6195ad5009aefb5bf2c2f93866
WuGenQiang <2422676183@qq.com>
2020-03-27 20:11:20 +0800

    auto update https://wugenqiang.github.io/Vue-Learning

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,17 @@ echo '--------upload files start--------'
 # git init
 git add .
 git status
-git commit -m 'auto update https://wugenqiang.github.io/CS-Notes'
+git commit -m 'auto update https://wugenqiang.github.io/Vue-Learning'
 echo '--------commit successfully--------'
 
 # git push -f https://github.com/wugenqiang/CS-Notes.git master
-git push -u https://github.com/wugenqiang/CS-Notes.git master
+git push -u https://github.com/wugenqiang/Vue-Learning.git master
 # git remote add origin https://github.com/wugenqiang/CS-Notes.git
 # git push -u origin master
 echo '--------push to GitHub successfully--------'
 
 # git push -f https://gitee.com/wugenqiang/CS-Notes.git master
-git push -u https://gitee.com/wugenqiang/CS-Notes.git master
+git push -u https://gitee.com/wugenqiang/Vue-Learning.git master
 # git remote add origin https://gitee.com/wugenqiang/CS-Notes.git
 # git push -u origin master
 echo '--------push to Gitee successfully--------'
